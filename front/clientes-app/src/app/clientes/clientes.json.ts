7c5f427e051fb7b3885e612993451ee32e8e3e22
ManriqueCesar <38671208+ManriqueCesar@users.noreply.github.com>
2020-10-20 16:12:14 -0500

    fix de deploy

---
+++ front/clientes-app/src/app/clientes/clientes.json.ts
@@ -0,0 +1,17 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] =
+    [
+
+        { id: 1, nombre: 'Cesar', apellido: 'Manrique', email: 'cesar.manrique4@unmsm.edu.pe', createAt: '2020-05-05' },
+        { id: 2, nombre: 'Abraham', apellido: 'Mayanga', email: 'cmanrique@unmsm.edu.pe', createAt: '2020-02-05' },
+        { id: 3, nombre: 'Melinna', apellido: 'Rojas', email: 'mRojas@unmsm.edu.pe', createAt: '2019-05-05' },
+        { id: 4, nombre: 'Anyi', apellido: 'Rodriguez', email: 'aRodriguez@unmsm.edu.pe', createAt: '2019-05-05' },
+        { id: 5, nombre: 'Massiel', apellido: 'Olulo', email: 'mOlulo@unmsm.edu.pe', createAt: '2019-05-05' },
+        { id: 6, nombre: 'Patricia', apellido: 'Segil', email: 'pSegil@unmsm.edu.pe', createAt: '2015-05-05' },
+        { id: 7, nombre: 'Andrea', apellido: 'Vasquez', email: 'aVasquez@unmsm.edu.pe', createAt: '2015-05-05' },
+        { id: 8, nombre: 'Silvia', apellido: 'Eusebio', email: 'eUsebio@unmsm.edu.pe', createAt: '2018-05-05' },
+        { id: 9, nombre: 'Karen', apellido: 'Carrasco', email: 'kCarrasco@unmsm.edu.pe', createAt: '2010-05-05' },
+        { id: 10, nombre: 'Stefani', apellido: 'ga', email: 'sGa@unmsm.edu.pe', createAt: '2018-05-05' },
+        { id: 11, nombre: 'Nahomi', apellido: 'Pacheco', email: 'nPacheco@unmsm.edu.pe', createAt: '2006-05-05' }
+    ];
